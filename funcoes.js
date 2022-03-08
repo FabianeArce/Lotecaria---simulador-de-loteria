5045fc1d0c27f0917c7f6c3d0e52016edff8d7d5
FabianeArce <fabianearce3@gmail.com>
2022-03-08 18:57:20 -0300

    conserto da função

--- funcoes.js
+++ funcoes.js
@@ -16,18 +16,25 @@ function sorteioNum(){
 }
 
  function getNum(valor,pos){
-     valo = Numbe(valor)
+     valor = Number(valor)
   if(valor>60){
       alert("O valor é maior que 60")
+      limpar(pos)
   }else if(valor <= 0){
       alert("O valor não pode ser menor que 0")
+      limpar(pos)
   }else if(numDig.includes(valor)){
       alert("O valor já foi digitado")
+      limpar(pos)
   }
   else{
       numDig[pos] = valor
   }
  }
+
+  function limpar(pos){
+      document.getElementById(`num${pos}`).value = ""
+  }
 
  function verificaAcertos(){
      let cont = 0
