5b2d3b4ec4187dee7c8fa562c312c31f268399ad
FabianeArce <fabianearce3@gmail.com>
2022-03-08 18:26:58 -0300

    criando funções importantes

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,5 @@
 var numSort = []
+var numDig = []
 
 function sorteioNum(){
     numSort = []
@@ -10,5 +11,30 @@ function sorteioNum(){
 
     }while(numSort.length < 6);
 
-    console.log(numSort)
+    document.getElementById("numsort").innerText = numSort
+    verificaAcertos()
 }
+
+ function getNum(valor,pos){
+     valo = Numbe(valor)
+  if(valor>60){
+      alert("O valor é maior que 60")
+  }else if(valor <= 0){
+      alert("O valor não pode ser menor que 0")
+  }else if(numDig.includes(valor)){
+      alert("O valor já foi digitado")
+  }
+  else{
+      numDig[pos] = valor
+  }
+ }
+
+ function verificaAcertos(){
+     let cont = 0
+     numDig.forEach(function(valor,index){
+         if(numSort.includes(valor)){
+             cont = cont+1
+         }
+     })
+   document.getElementById("total").innerText = cont
+ }
